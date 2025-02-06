87241038e0332d2ca1348844862a16ae7d0b5a10
SzurmaiBence0413 <szurmaibence56@gmail.com>
2025-02-06 14:15:07 +0100

    7. feladat eleje van, kommenteles kesz

--- Helyjegy_H2/UtazoIroda.cs
+++ Helyjegy_H2/UtazoIroda.cs
@@ -11,7 +11,7 @@ namespace Helyjegy_H2
     {
         private string fajl;
         List<Utas> utazokLista = new List<Utas>();
-           List<int> megallokLista = new List<int>();
+        List<int> megallokLista = new List<int>();
 
 
 
@@ -21,21 +21,23 @@ namespace Helyjegy_H2
             Beolvas();
         }
 
+        //Bevétel kiszámolása
         internal void Bevetel()
         {
             int ar = 0;
             int Bevetel = 0;
-            foreach (var utas  in utazokLista )
+            foreach (var utas in utazokLista)
             {
                 ar += utas.tav / 10 * Utazas.fizetendo;
                 int kerekitettAr = OtreKerekito(ar);
-                Bevetel+= kerekitettAr;
+                Bevetel += kerekitettAr;
             }
 
 
-            Console.WriteLine("Ennyi bevétel származott a társaságnak a jegyekből: {0} Ft",Bevetel);
+            Console.WriteLine("Ennyi bevétel származott a társaságnak a jegyekből: {0} Ft", Bevetel);
         }
 
+        //Ötre kerekítő függvény
         private int OtreKerekito(int ar)
         {
             int maradék = ar % 5;
@@ -54,24 +56,25 @@ namespace Helyjegy_H2
 
         internal void LegutolsoJegyvasarloAdatai()
         {
-            int utolsoUtasIndexe = utazokLista.Count - 1;
+
+            int utolsoUtasIndexe = utazokLista.Count - 1; // Utolsó jegyvásárló indexe
             for (int i = 0; i < utazokLista.Count; i++)
             {
                 if (i == utolsoUtasIndexe)
                 {
-                    Console.WriteLine("ülés sorszáma: {0}, beutazott távolság {1}", utazokLista[i].ulesId, utazokLista[i].tav);
+                    Console.WriteLine("ülés sorszáma: {0}, beutazott távolság {1}", utazokLista[i].ulesId, utazokLista[i].tav); // Utolsó jegyvásárló álltal beutazott távolság és a ülésének sorszámát kiírjuk
                 }
-            }  
+            }
 
-          
-          
+
+
         }
 
-     
+
 
         internal void TejlesUtatVegigUtaztak()
         {
-            List<int> sorszamok = new List<int>();
+            List<int> sorszamok = new List<int>(); // ülések sorszámának listája
             int teljesTav = Utazas.vonalHossz;
             foreach (var utas in utazokLista)
             {
@@ -83,7 +86,7 @@ namespace Helyjegy_H2
 
             foreach (var sorszam in sorszamok)
             {
-                Console.Write("{0} ",sorszam);
+                Console.Write("{0} ", sorszam);
             }
         }
 
@@ -92,7 +95,7 @@ namespace Helyjegy_H2
             string[] sorok = File.ReadAllLines(fajl);
             for (int i = 0; i < sorok.Length; i++)
             {
-                if (i == 0)
+                if (i == 0) // ez elsősor kivétele és az utázás adatait Utazás abstact oszályba rakjuk bele
                 {
                     string[] oszlopok = sorok[i].Split(' ');
                     int eladottjegyekSzama = int.Parse(oszlopok[0]);
@@ -111,41 +114,42 @@ namespace Helyjegy_H2
                     int felszall = int.Parse(oszlopok[1]);
                     int leszall = int.Parse(oszlopok[2]);
                     int tav = leszall - felszall;
-                    utazokLista.Add(new Utas(ules, felszall, leszall,tav));
+                    utazokLista.Add(new Utas(ules, felszall, leszall, tav));
                 }
-            } 
+            }
 
         }
 
         internal void UtolsoElottiMegalloAdatai()
         {
             int utolsoElottiLe = (from i in utazokLista  //utolsó előtti megálló, ahol leszálló volt
-                                    orderby i.leszall
-                                    where i.leszall != Utazas.vonalHossz
-                                    select i.leszall).Last();
+                                  orderby i.leszall
+                                  where i.leszall != Utazas.vonalHossz
+                                  select i.leszall).Last();
 
             int utolsoElottiFel = (from i in utazokLista  //utolsó előtti megálló, ahol felszálló volt
-                                     orderby i.felszall
-                                     where i.felszall != Utazas.vonalHossz
-                                     select i.felszall).Last();
+                                   orderby i.felszall
+                                   where i.felszall != Utazas.vonalHossz
+                                   select i.felszall).Last();
 
             int utolsEllotti = Math.Max(utolsoElottiLe, utolsoElottiFel); //utolsó előtti megálló ahol leszálló és/vagy felszálló volt
 
             var leSzallokSzama = (from i in utazokLista
-                           where i.leszall == utolsEllotti
-                           select i).Count();
+                                  where i.leszall == utolsEllotti
+                                  select i).Count();
 
-           
+
 
             var felSZallokSzama = (from i in utazokLista
-                            where i.felszall == utolsEllotti
-                            select i).Count();
+                                   where i.felszall == utolsEllotti
+                                   select i).Count();
 
             Console.WriteLine("Utolsó állomás előtt felszállók száma: {0} fő, leszállók száma {1}", felSZallokSzama, leSzallokSzama);
         }
 
         internal void MegallokSzama()
         {
+
             var megallokSzama = (from i in utazokLista select i.felszall).Union(from i in utazokLista select i.leszall);
             Console.WriteLine("A busznak {0} db megállója volt!", megallokSzama.Count() - 2);
 
@@ -155,6 +159,12 @@ namespace Helyjegy_H2
         {
             Console.Write("Adj meg egy távolságot: ");
             int bekertTavolsag = int.Parse(Console.ReadLine());
+            string celFajlNeve = "kihol.txt";
+            List<Utas> KeresettUtazokLista = new List<Utas>();
+
+            
+                
+           
         }
     }
 }
